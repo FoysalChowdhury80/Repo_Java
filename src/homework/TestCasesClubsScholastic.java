b5a7a05724236e37ae053c8553e507f0cc1aa69b
foysal chowdhury <mamtawahid@gmail.com>
2022-03-08 19:17:17 -0500

    Homework3

---
+++ src/homework/TestCasesClubsScholastic.java
@@ -0,0 +1,98 @@
+package homework;
+
+public class TestCasesClubsScholastic {
+
+    public static void main(String[] args) {
+        /**
+         * Scenarion: To verify user can create a teacher account
+         *
+         * 1. Verify user can create a teacher account
+         *
+         * Given: Go to cubs.scholastic.com
+         * When: User clicks on get started button
+         *      And: User selects create an account option
+         *      And: User selects teache/administrator option and select next button
+         *      And: User provides title as Mr.
+         *      And: First name as AA
+         *      And: Last name as Ch
+         *      And: Email address as abcg@gmail.com
+         *      And: Password as abc12345
+         *      And: Check mark terms and condition box
+         *      And: Click on Next button
+         *      And: Enter zip code as 10002
+         *      And: Click on Search button
+         *      And: User selects schools from drop down
+         *      And: Click on Next button
+         *      And: Select Role as teacher by grade
+         *      And: Select Grade as Early Child hood
+         *      And: Click on next
+         *      And: Select number of students
+         *      And: Click on next
+         *      And Select prefered reading level
+         *      And; Select Continue
+         *
+         * Then: Very that the teacher account has been created successfully
+         *
+         *
+         *Scenario: User is able to sign in with a valid username and password
+         *
+         *Scenario: User is not able to sign in with wrong username correct password
+         *
+         *Scenario: User is not able to sign in with correct username but wrong password
+         *
+         *Scenario: User is not able to sign in with wrong username and wrong password
+         *
+         *
+         *
+         */
+
+
+        /**
+         * Wish List
+         *
+         * Scenarion: Teacher can add a books into the wish list
+         *
+         * Scenario: Teacher can create wish list from top next to Name or by clicking on add books button in the the wish list
+         *
+         * Scenario: Teacher can add desired books to the wish list by selecting books using search box
+         *
+         * Scenario: Teacher can add listed book to his/her cart
+         *
+         * Scenario: Teacher Teacher Can delete book from the wish list
+         *
+         * Scenario: Teacher can move books from the wishlist to the options available
+         *
+         * Scenario: Teacher can add books to wish list from suggestions of books
+         *
+         * Scenario: Teacher can add any quantity of books/book in the wish list
+         *
+         * Scenario: Teacher can add a note for the wish listed book
+         *
+         * Scenario: Teacher can edit the note for the listed book
+         *
+         * Scenario: Wish listed items and quantity and prices is displayed correctly
+         *
+         * Scenario: Teacher is able to preview the wish list
+         *
+         * Scenario: Teacher is able to on or off for the publication option of the wish list
+         *
+         * Scenario: Teacher is able to change the quantity of the listed item
+         *
+         * Scenario: Teacher is able to delete a listed item by changing quantity to 0
+         *
+         * Scenario: Teacher is able to delete/keep books by selecting Yes/No option from the pop up displayed after selecting delete option
+         *
+         * Scenario: When Teacher selects 0 quantity it should pop up with Yes and No option
+         *
+         * Scenario: Teacher is able to move to wish list and select quantity from save later list
+         *
+         * Scenario: Teacher is able to move to wish list and can select quantity from recommended list
+         *
+         *
+         *
+         *
+         *
+         *
+         */
+    }
+}
